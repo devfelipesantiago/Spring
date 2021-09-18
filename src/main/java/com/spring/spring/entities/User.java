ffdd399df8b447b4858d094e3a0d56b3079fed70
Felipe <felipe@pop-os.localdomain>
2021-09-18 16:17:21 -0300

    Create user controller

---
+++ src/main/java/com/spring/spring/entities/User.java
@@ -0,0 +1,120 @@
+package com.spring.spring.entities;
+
+import org.yaml.snakeyaml.constructor.Construct;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+  private Long id;
+  private String name;
+  private String email;
+  private String phone;
+  private String password;
+
+  public User() {
+  }
+
+  public User(Long id, String name, String email, String phone, String password) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.password = password;
+  }
+
+  /**
+   * @return Long return the id
+   */
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * @param id the id to set
+   */
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /**
+   * @return String return the name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * @param name the name to set
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * @return String return the email
+   */
+  public String getEmail() {
+    return email;
+  }
+
+  /**
+   * @param email the email to set
+   */
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  /**
+   * @return String return the phone
+   */
+  public String getPhone() {
+    return phone;
+  }
+
+  /**
+   * @param phone the phone to set
+   */
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  /**
+   * @return String return the password
+   */
+  public String getPassword() {
+    return password;
+  }
+
+  /**
+   * @param password the password to set
+   */
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof User)) return false;
+    User user = (User) o;
+    return Objects.equals(id, user.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", email='" + email + '\'' +
+            ", phone='" + phone + '\'' +
+            ", password='" + password + '\'' +
+            '}';
+  }
+}
---
+++ src/main/java/com/spring/spring/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.spring.spring.resources;
+
+import com.spring.spring.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L,
+                "Maria",
+                "maria@mail.com",
+                "99999999",
+                "123456");
+        return ResponseEntity.ok().body(u);
+    }
+}
