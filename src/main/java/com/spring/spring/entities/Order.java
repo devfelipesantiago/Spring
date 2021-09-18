2994d530902df3489ca7554a134d63ce61916c2d
Felipe <felipe@pop-os.localdomain>
2021-09-18 18:05:50 -0300

    Create Enums

--- src/main/java/com/spring/spring/entities/Order.java
+++ src/main/java/com/spring/spring/entities/Order.java
@@ -1,5 +1,7 @@
 package com.spring.spring.entities;
 
+import com.spring.spring.entities.enums.OrderStatus;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
@@ -15,6 +17,8 @@ public class Order implements Serializable {
     private Long id;
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -22,9 +26,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -50,6 +55,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
