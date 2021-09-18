558fe9e85d9a2f2950e50a71611e108be559498a
Felipe <felipe@pop-os.localdomain>
2021-09-18 17:19:53 -0300

    Create class Order

--- src/main/java/com/spring/spring/entities/User.java
+++ src/main/java/com/spring/spring/entities/User.java
@@ -2,14 +2,14 @@ package com.spring.spring.entities;
 
 import org.yaml.snakeyaml.constructor.Construct;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Table(name = "td_user")
 public class User implements Serializable {
 
   @Id
@@ -19,6 +19,9 @@ public class User implements Serializable {
   private String email;
   private String phone;
   private String password;
+
+  @OneToMany(mappedBy = "client")
+  private List<Order> orders = new ArrayList<>();
 
   public User() {
   }
@@ -99,6 +102,10 @@ public class User implements Serializable {
    */
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  public List<Order> getOrders() {
+    return orders;
   }
 
   @Override
